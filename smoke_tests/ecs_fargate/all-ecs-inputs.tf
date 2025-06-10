e6fb8a29eca3ffa4eda035d85d7b2dffff864e15
Gabriel Dos Santos <gabe.dossantos@datadoghq.com>
2025-06-10 15:33:00 -0400

    Prefix roles/policies with terraform-test

--- smoke_tests/ecs_fargate/all-ecs-inputs.tf
+++ smoke_tests/ecs_fargate/all-ecs-inputs.tf
@@ -32,7 +32,7 @@ resource "aws_efs_access_point" "fs" {
 }
 
 resource "aws_iam_role" "ecs_task_role" {
-  name = "ecs_task_role"
+  name = "terraform_test_ecs_task_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -49,7 +49,7 @@ resource "aws_iam_role" "ecs_task_role" {
 }
 
 resource "aws_iam_policy" "ecs_task_policy" {
-  name        = "ecs_task_policy"
+  name        = "terraform_test_ecs_task_policy"
   description = "Policy for ECS task role to access EFS"
   policy = jsonencode({
     Version = "2012-10-17"
