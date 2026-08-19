b4cf7d00146dff2e167b43403c6a2184ef442e19
Mathew Estafanous <56979977+Mathew-Estafanous@users.noreply.github.com>
2026-08-19 15:28:07 -0400

    chore: release 1.1.1 for ecs modules (#89)

--- modules/ecs_ec2/datadog.tf
+++ modules/ecs_ec2/datadog.tf
@@ -35,7 +35,7 @@ locals {
 # Version and Install Info
 locals {
   # Datadog ECS task tags
-  version = "1.1.0"
+  version = "1.1.1"
 
   install_info_tool              = "terraform"
   install_info_tool_version      = "terraform-aws-ecs-datadog"
